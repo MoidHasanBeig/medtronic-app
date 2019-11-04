06e2db7eea0f86ebe55ea33e554f2acb986358f6
MoidHasanBeig <mhbdgr8@gmail.com>
2019-11-04 20:52:58 +0530

    Pre final

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const ejs = require('ejs');
 
 const app = express();
+
+let query = ['Quotation','Demo','Sales Person meeting'];
+
+app.use(express.static(__dirname + "/public"));
+app.set('view engine', 'ejs');
+
 
 app.listen(3000, () => {
   console.log("Server is live @ 3000");
@@ -11,9 +16,11 @@ app.listen(3000, () => {
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req,res) => {
-
+  res.render("index", {userQuery:query});
 });
 
 app.post("/", (req,res) => {
-
+  console.log(req.body);
+  // alert("You have successfully raised your query.");
+  res.redirect("/");
 });
