68dbb010657f1b65107891a736488d18aa50e66e
MoidHasanBeig <mhbdgr8@gmail.com>
2019-11-04 22:47:11 +0530

    Notification added

--- app.js
+++ app.js
@@ -7,20 +7,21 @@ let query = ['Quotation','Demo','Sales Person meeting'];
 
 app.use(express.static(__dirname + "/public"));
 app.set('view engine', 'ejs');
-
+app.use('/node_modules', express.static(__dirname + '/node_modules'))
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(3000, () => {
   console.log("Server is live @ 3000");
 });
 
-app.use(bodyParser.urlencoded({extended:true}));
-
 app.get("/", (req,res) => {
-  res.render("index", {userQuery:query});
+  console.log(req.query.message);
+  let obj = {userQuery:query, message : req.query.message}
+  res.render("index", obj);
 });
 
 app.post("/", (req,res) => {
   console.log(req.body);
-  // alert("You have successfully raised your query.");
-  res.redirect("/");
+  let message = "Hey  we got you"
+  res.redirect(`/?message=${message}`);
 });
